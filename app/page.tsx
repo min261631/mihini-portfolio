5b727edea79f12256e017ac22b6d164a26316c0a
Mihini <mihininiweka@gmail.com>
2026-09-20 16:51:37 +0930

    Move About introduction above selected work

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/components/Hero";
+import AboutIntro from "@/components/AboutIntro";
 import Projects from "@/components/Projects";
 import About from "@/components/About";
 
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <main>
       <Hero />
+      <AboutIntro />
       <Projects />
       <About />
     </main>
