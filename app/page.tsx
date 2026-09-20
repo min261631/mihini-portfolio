afc1b1f9743e041b2c7260d836fed4b626abca8c
Mihini <mihininiweka@gmail.com>
2026-09-20 16:55:00 +0930

    Keep homepage focused on hero and work

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 import Hero from "@/components/Hero";
-import AboutIntro from "@/components/AboutIntro";
 import Projects from "@/components/Projects";
-import About from "@/components/About";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <AboutIntro />
       <Projects />
-      <About />
     </main>
   );
 }
