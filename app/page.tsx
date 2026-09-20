08d3a91431aca9dfe6a5ac6d3bd023189867679d
Mihini <mihininiweka@gmail.com>
2026-09-20 16:48:28 +0930

    Add About section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
+import About from "@/components/About";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Projects />
+      <About />
     </main>
   );
 }
